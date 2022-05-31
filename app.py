f4387ecc99205eb6035c86d0c1023839fd75b82c
Skyflex <jose.gonzalezuutt@gmail.com>
2022-05-31 18:24:54 -0500

    We add five coins

--- app.py
+++ app.py
@@ -3,17 +3,18 @@
 from xmlrpc.client import boolean
 
 
-coins = (.01,.10,.15,.50)
-cent, dime, quarter, half = coins
+coins = (.01,.05,.10,.15,.50)
+cent, five, dime, quarter, half = coins
 
 print(cent)
 
 while True:
     cents=int(input("how many CENTS do you have? "))
+    fives=int(input("how many FIVES do you have? "))
     dimes=int(input("How many DIMES do you have? "))
     quarters=int(input("How many QUARTERS do you have? "))
     halfs=int(input("How many HALFS do you have? "))
-    money=(cents*cent)+(dimes*dime)+(quarters*quarter)+(halfs*half)
+    money=(cents*cent)+(fives*five)+(dimes*dime)+(quarters*quarter)+(halfs*half)
     print("You have: ", money)
 
     answer=input("Did you finish? YES or NOT : ")
